11dc1dbc7a5763d52c7ef9000ee3895fbb7ad748
Node7DevelopMentStudios <braden.townsend98@hotmail.com>
2026-08-03 15:44:53 -0700

    Add files via upload

---
+++ node7-banking/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+game 'rdr3'
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+lua54 'yes'
+
+name 'node7-banking'
+author 'NODE7 Development Studios'
+description 'NODE7 personal, society, gang, and shared banking powered exclusively by node7-ui exports.'
+version '2.3.0'
+
+files {
+    'locales/*.json'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    'node7-core',
+    'node7-ui'
+}
